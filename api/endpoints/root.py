d678122388fca00c0fad886de643e9cdffa2d463
Gyula Győr <gygyor@gmail.com>
2021-10-25 14:56:54 +0200

    x_properties fix

--- api/endpoints/root.py
+++ api/endpoints/root.py
@@ -7,7 +7,7 @@ from I4cAPI import I4cApiRouter
 router = I4cApiRouter()
 
 
-@router.get("/login", response_model=models.users.UserResponse, x_properties=dict(object="user", action="get"))
+@router.get("/login", response_model=models.users.UserResponse, x_properties=dict(object="user", action="login"))
 async def login(
         credentials: HTTPBasicCredentials = Depends(common.security_checker("get/login"))):
     "Get user info based on login name"
