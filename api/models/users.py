308dc18f82863c944fa8de1eec08862be0533b4b
Gyula Győr <gygyor@gmail.com>
2021-10-25 15:26:48 +0200

    /login with roles and roles_eff

--- api/models/users.py
+++ api/models/users.py
@@ -41,19 +41,50 @@ def user_from_row(row):
 
 
 async def login(credentials: HTTPBasicCredentials, *, pconn=None):
-    sql = dedent("""\
+    sql_user = dedent("""\
             select *
-            from public."user" u
-            where 
-              u."login_name" = $1
+            from "user"
+            where "user".login_name = $1
+          """)
+
+    sql_roles = dedent("""\
+            select user_role.role
+            from user_role 
+            where user_role."user" = $1
+          """)
+
+    sql_roles_eff = dedent("""\
+            with
+              recursive deep_role_r as
+                (select distinct "name" as toprole, "name" as midrole, "name" as subrole from "role"
+                 union
+                 select deep_role_r.toprole, role_subrole.role as midrole, role_subrole.subrole
+                 from deep_role_r 
+                 join role_subrole on deep_role_r."subrole" = role_subrole."role"),
+              deep_role as (select distinct toprole as role, subrole from deep_role_r)
+            select distinct 
+              deep_role.subrole as "role"
+            from user_role 
+            join deep_role on deep_role.role = user_role."role"
+            join role_grant on deep_role.subrole = role_grant."role"
+            where user_role."user" = $1
           """)
     async with DatabaseConnection(pconn) as conn:
-        res = await conn.fetchrow(sql, credentials.username)
-        if not res:
+        db_user = await conn.fetchrow(sql_user, credentials.username)
+        if not db_user:
             raise HTTPException(status_code=500, detail="User record not found")
-        res = user_from_row(res)
-        # todo 1: *****
-        res["roles"] = []
-        res["roles_eff"] = []
-        return {"user": res}
+        res = user_from_row(db_user)
 
+        res_roles = []
+        db_roles = await conn.fetchrow(sql_roles, res["id"])
+        for r in db_roles:
+            res_roles.append(r)
+        res["roles"] = res_roles
+
+        res_roles_eff = []
+        db_roles_eff = await conn.fetchrow(sql_roles_eff, res["id"])
+        for r in db_roles_eff:
+            res_roles_eff.append(r)
+        res["roles_eff"] = res_roles_eff
+
+        return {"user": res}
