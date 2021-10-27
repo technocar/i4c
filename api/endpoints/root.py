00335178d1de8cbdeb43f5aabb15a5742298c24b
Gyula Győr <gygyor@gmail.com>
2021-10-27 17:50:49 +0200

    tag correction

--- api/endpoints/root.py
+++ api/endpoints/root.py
@@ -7,7 +7,7 @@ from I4cAPI import I4cApiRouter
 router = I4cApiRouter()
 
 
-@router.get("/login", response_model=models.users.UserResponse, tags=["log"], x_properties=dict(object="user", action="login"))
+@router.get("/login", response_model=models.users.UserResponse, tags=["user"], x_properties=dict(object="user", action="login"))
 async def login(
         credentials: HTTPBasicCredentials = Depends(common.security_checker("get/login"))):
     "Get user info based on login name"
