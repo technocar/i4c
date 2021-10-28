a017b05901309692b7d23b11b64b892bda6882d3
Gyula Győr <gygyor@gmail.com>
2021-10-28 11:23:11 +0200

    more logs

--- api/common/db_pool.py
+++ api/common/db_pool.py
@@ -1,27 +1,35 @@
 import asyncpg
-from .common_obj import dbcfg
+from .common_obj import dbcfg, log
 
 
 class DatabaseConnection:
     db_pool = None
-    __slots__ = ['pconn', 'conn']
+    conn_id_generator = 0
+    __slots__ = ['pconn', 'conn', 'conn_id']
 
     def __init__(self, pconn=None):
         self.pconn = pconn
         self.conn = None
+        self.conn_id = None
 
     async def __aenter__(self):
         if self.pconn is not None:
             self.conn = self.pconn
         else:
+            DatabaseConnection.conn_id_generator += 1
+            self.conn_id = DatabaseConnection.conn_id_generator
+            log.debug(f"acquiring db connection {self.conn_id}")
             self.conn = await DatabaseConnection.db_pool.acquire()
+            log.debug(f"acquired db connection {self.conn_id}")
         return self.conn
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
         if self.pconn is None:
+            log.debug(f"releasing db connection {self.conn_id}")
             await DatabaseConnection.db_pool.release(self.conn)
+            log.debug(f"released db connection {self.conn_id}")
 
     @staticmethod
     async def init_db_pool():
         DatabaseConnection.db_pool = await asyncpg.create_pool(**dbcfg)
-        print("db_pool inited.")
+        log.info("db_pool inited.")
