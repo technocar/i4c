50b0b8a02b49689d2636a7bb2f3340f79628e26d
Gyula Győr <gygyor@gmail.com>
2021-11-12 16:19:53 +0100

    api: alarmdef get/put

--- api/endpoints/alarm.py
+++ api/endpoints/alarm.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, Body, Path
+from fastapi import Depends, Body, Path, HTTPException
 from fastapi.security import HTTPBasicCredentials
 import common
 import models.alarm
@@ -7,9 +7,21 @@ from I4cAPI import I4cApiRouter
 router = I4cApiRouter(include_path="/alarm")
 
 
+@router.put("/defs/{name}", response_model=models.alarm.AlarmDef, x_properties=dict(object="alarmdef", action="put"))
+async def alarmdef_put(
+    credentials: HTTPBasicCredentials = Depends(common.security_checker("put/alarm/defs/{name}")),
+    name: str = Path(...),
+    alarm: models.alarm.AlarmDefIn = Body(...),
+):
+    return await models.alarm.alarmdef_put(credentials, name, alarm)
+
+
 @router.get("/defs/{name}", response_model=models.alarm.AlarmDef, x_properties=dict(object="alarmdef", action="get"))
 async def alarmdef_get(
     credentials: HTTPBasicCredentials = Depends(common.security_checker("get/alarm/defs/{name}")),
     name: str = Path(...),
 ):
-    return await models.alarm.alarmdef_get(credentials, name)
+    alarm_id, res = await models.alarm.alarmdef_get(credentials, name)
+    if alarm_id is None:
+        raise HTTPException(status_code=404, detail="No record found")
+    return res
