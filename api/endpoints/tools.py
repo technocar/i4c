5e3e1016356cb1bc9a431bb21472af88ae84065e
Gyula Győr <gygyor@gmail.com>
2021-11-16 17:07:28 +0100

    api: config

--- api/endpoints/tools.py
+++ api/endpoints/tools.py
@@ -48,7 +48,7 @@ async def patch_tools(
 @router.get("/list", response_model=List[models.tools.ToolDataPointType], x_properties=dict(object="tools", action="list"))
 async def tool_list(
         credentials: HTTPBasicCredentials = Depends(common.security_checker("get/log/find")),
-        device: Device = Query(None, title="device"),
+        device: Device = Query(..., title="device"),
         timestamp: Optional[datetime] = Query(None, description="eg.: 2021-08-15T15:53:11.123456Z"),
         max_count: Optional[int] = Query(None)):
     return await models.tools.tool_list(credentials, device, timestamp, max_count)
