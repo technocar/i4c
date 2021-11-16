e1c70c3804efe621a45266ddb815e5761f00ed76
Gyula Győr <gygyor@gmail.com>
2021-11-16 15:11:26 +0100

    api: more tools api

--- api/endpoints/tools.py
+++ api/endpoints/tools.py
@@ -1,4 +1,6 @@
-from fastapi import Depends, Body, HTTPException, Path
+from typing import List
+
+from fastapi import Depends, Body, Path
 from fastapi.security import HTTPBasicCredentials
 import common
 import models.log
@@ -8,31 +10,40 @@ from I4cAPI import I4cApiRouter
 router = I4cApiRouter(include_path="/tools")
 
 
-def check_allow_tool_log_data_id(data_id):
-    if data_id not in ('install_tool', 'remove_tool'):
-        raise HTTPException(status_code=400, detail="Allowed data_ids are 'install_tool' and 'remove_tool'")
-
-
-@router.put("", status_code=201, x_properties=dict(object="tool", action="log_write"))
+@router.put("", status_code=201, x_properties=dict(object="tool", action="put"))
 async def tools_log_write(
         credentials: HTTPBasicCredentials = Depends(common.security_checker("put/tools")),
-        datapoint: models.log.DataPoint = Body(...)):
-    check_allow_tool_log_data_id(datapoint.data_id)
-    return await models.log.put_log_write(credentials, [datapoint], override=True)
+        datapoint: models.tools.ToolDataPoint = Body(...)):
+    d = models.log.DataPoint(timestamp=datapoint.timestamp,
+                             sequence=datapoint.sequence,
+                             device=datapoint.device,
+                             data_id=datapoint.data_id,
+                             value_text=datapoint.tool_id,
+                             value_extra=datapoint.slot_number)
+    return await models.log.put_log_write(credentials, [d], override=True)
 
 
-@router.delete("", status_code=201, x_properties=dict(object="tool", action="log_delete"))
+@router.delete("", status_code=200, x_properties=dict(object="tool", action="delete"))
 async def tools_log_delete(
         credentials: HTTPBasicCredentials = Depends(common.security_checker("delete/tools")),
-        datapointkey: models.log.DataPointKey = Body(...)):
-    check_allow_tool_log_data_id(datapointkey.data_id)
-    return await models.log.delete_log(credentials, datapointkey)
+        datapointkey: models.tools.ToolDataPointKey = Body(...)):
+    d = models.log.DataPointKey(timestamp=datapointkey.timestamp,
+                                sequence=datapointkey.sequence,
+                                device=datapointkey.device,
+                                data_id=datapointkey.data_id)
+    return await models.log.delete_log(credentials, d)
 
 
-@router.patch("/{tool_id}", response_model=models.common.PatchResponse, x_properties=dict(object="projects ver", action="patch"))
+@router.patch("/{tool_id}", response_model=models.common.PatchResponse, x_properties=dict(object="tools", action="patch"))
 async def patch_tools(
     credentials: HTTPBasicCredentials = Depends(common.security_checker("patch/tools/{tool_id}")),
     tool_id: str = Path(...),
     patch: models.tools.ToolsPatchBody = Body(...),
 ):
     return await models.tools.patch_project_version(credentials, tool_id, patch)
+
+
+@router.get("/list", response_model=List[models.tools.ToolItem], x_properties=dict(object="tools", action="list"))
+async def tool_list(
+        credentials: HTTPBasicCredentials = Depends(common.security_checker("get/tools/list"))):
+    return await models.tools.tool_list(credentials)
--- api/models/tools.py
+++ api/models/tools.py
@@ -1,7 +1,10 @@
+from datetime import datetime, date, timedelta
+from enum import Enum
 from textwrap import dedent
 from typing import Optional
 from fastapi import HTTPException
 from common import I4cBaseModel, DatabaseConnection
+from models.common import PatchResponse
 
 
 class ToolsPatchChange(I4cBaseModel):
@@ -15,6 +18,33 @@ class ToolsPatchBody(I4cBaseModel):
     change: ToolsPatchChange
 
 
+class ToolDataId(str, Enum):
+    install_tool = "install_tool"
+    remove_tool = "remove_tool"
+
+
+class ToolDataPointKey(I4cBaseModel):
+    timestamp: datetime
+    sequence: int
+    device: str
+    data_id: ToolDataId
+
+
+class ToolDataPoint(I4cBaseModel):
+    timestamp: datetime
+    sequence: int
+    device: str
+    data_id: ToolDataId
+    tool_id: Optional[str]
+    slot_number: Optional[str]
+
+
+class ToolItem(I4cBaseModel):
+    tool_id: Optional[str]
+    type: Optional[str]
+    count: int
+
+
 async def patch_project_version(credentials, tool_id, patch:ToolsPatchBody, *, pconn=None):
     try:
         async with DatabaseConnection(pconn) as conn:
@@ -25,5 +55,24 @@ async def patch_project_version(credentials, tool_id, patch:ToolsPatchBody, *, p
                       SET \"type\" = EXCLUDED.\"type\";
                       """)
             await conn.execute(sql, tool_id, patch.change.type)
+            return PatchResponse(changed=True)
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Sql error: {e}")
+
+
+async def tool_list(credentials):
+    async with DatabaseConnection() as conn:
+        sql = dedent("""\
+                select
+                  l.value_text "tool_id",
+                  t.type,
+                  count(*) "count"
+                from public.log l
+                left join tools t on t.id = l.value_text
+                where
+                  l.timestamp >= $1::timestamp with time zone -- */ '2021-08-23 07:56:00.957133+02'::timestamp with time zone
+                  and l.data_id in ('install_tool', 'remove_tool')
+                group by l.value_text, t.type
+                order by 1,2
+                """)
+        return await conn.fetch(sql, date.today() + timedelta(days=-365))
