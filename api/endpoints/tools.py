5a47c98a22d50000eb7bcf745090144e98c6bd26
Krisztián Pintér <pinterkr@gmail.com>
2021-11-29 18:13:41 +0100

    some docs and cli defs

--- api/endpoints/tools.py
+++ api/endpoints/tools.py
@@ -16,6 +16,9 @@ router = I4cApiRouter(include_path="/tools")
 async def tools_log_write(
         credentials: HTTPBasicCredentials = Depends(common.security_checker("put/tools")),
         datapoint: models.tools.ToolDataPoint = Body(...)):
+    """
+    Records a tool change event.
+    """
     d = models.log.DataPoint(timestamp=datapoint.timestamp,
                              sequence=datapoint.sequence,
                              device=datapoint.device,
@@ -29,6 +32,9 @@ async def tools_log_write(
 async def tools_log_delete(
         credentials: HTTPBasicCredentials = Depends(common.security_checker("delete/tools")),
         datapointkey: models.tools.ToolDataPointKey = Body(...)):
+    """
+    Removes a tool change event.
+    """
     d = models.log.DataPointKey(timestamp=datapointkey.timestamp,
                                 sequence=datapointkey.sequence,
                                 device=datapointkey.device,
@@ -36,18 +42,21 @@ async def tools_log_delete(
     return await models.log.delete_log(credentials, d)
 
 
-@router.patch("/{tool_id}", response_model=models.common.PatchResponse, x_properties=dict(object="tools", action="patch"))
+@router.patch("/{tool_id}", response_model=models.common.PatchResponse, x_properties=dict(object="tool", action="patch"))
 async def patch_tools(
     credentials: HTTPBasicCredentials = Depends(common.security_checker("patch/tools/{tool_id}")),
     tool_id: str = Path(...),
     patch: models.tools.ToolsPatchBody = Body(...),
 ):
+    """
+    Updates a tool change event.
+    """
     return await models.tools.patch_project_version(credentials, tool_id, patch)
 
 
-@router.get("/list", response_model=List[models.tools.ToolDataPointType], x_properties=dict(object="tools", action="list"))
+@router.get("", response_model=List[models.tools.ToolDataPointType], x_properties=dict(object="tool", action="list"))
 async def tool_list(
-        credentials: HTTPBasicCredentials = Depends(common.security_checker("get/log/find")),
+        credentials: HTTPBasicCredentials = Depends(common.security_checker("get/tools")),
         device: Device = Query(..., title="device"),
         timestamp: Optional[datetime] = Query(None, description="eg.: 2021-08-15T15:53:11.123456Z"),
         sequence: Optional[int] = Query(None, description="sequence excluding this"),
@@ -55,7 +64,7 @@ async def tool_list(
     return await models.tools.tool_list(credentials, device, timestamp, sequence, max_count)
 
 
-@router.get("/list_usage", response_model=List[models.tools.ToolItem], x_properties=dict(object="tools", action="list_usage"))
+@router.get("/list_usage", response_model=List[models.tools.ToolItem], x_properties=dict(object="tool", action="usage"))
 async def tool_list_usage(
         credentials: HTTPBasicCredentials = Depends(common.security_checker("get/tools/list_usage"))):
     return await models.tools.tool_list_usage(credentials)
