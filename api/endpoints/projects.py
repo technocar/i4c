ef12f4d6359cbe2590b408d1d99f3b9e34a136d8
Krisztián Pintér <pinterkr@gmail.com>
2021-12-08 16:25:41 +0100

    doc and x-props

--- api/endpoints/projects.py
+++ api/endpoints/projects.py
@@ -18,6 +18,7 @@ async def list_projects(
     status: Optional[ProjectStatusEnum] = Query(None),
     file: Optional[str] = Query(None, description="Full savepath of the file.")
 ):
+    """Get a filtered list of projects."""
     file = models.projects.ProjFile.check_savepath(file)
     return await models.projects.get_projects(credentials, name, name_mask, status, file)
 
@@ -27,6 +28,7 @@ async def get_project(
     credentials: HTTPBasicCredentials = Depends(common.security_checker("get/projects/{name}")),
     name: str = Path(...),
 ):
+    """Retrieve a project by name"""
     res = await models.projects.get_projects(credentials, name)
     if len(res) > 0:
         return res[0]
@@ -38,6 +40,7 @@ async def new_project(
     credentials: HTTPBasicCredentials = Depends(common.security_checker("post/projects")),
     project: models.projects.ProjectIn = Body(...),
 ):
+    """Create a new project"""
     return await models.projects.new_project(credentials, project)
 
 
@@ -47,34 +50,38 @@ async def patch_project(
     name: str = Path(...),
     patch: models.projects.ProjectPatchBody = Body(...),
 ):
+    """Change a project if conditions are met"""
     return await models.projects.patch_project(credentials, name, patch)
 
 
-@router.get("/{name}/v/{ver}", response_model=models.projects.ProjectVersion, x_properties=dict(object="projectver", action="get"))
+@router.get("/{name}/v/{ver}", response_model=models.projects.ProjectVersion, x_properties=dict(object="project", action="getver"))
 async def list_projects_version(
     credentials: HTTPBasicCredentials = Depends(common.security_checker("get/projects/{name}/v/{ver}")),
     name: str = Path(...),
     ver: int = Path(...)
 ):
+    """Retrieve a project version"""
     res, _ = await models.projects.get_projects_version(credentials, name, ver)
     return res
 
 
-@router.post("/{name}/v", response_model=models.projects.ProjectVersion, x_properties=dict(object="projectver", action="new"))
+@router.post("/{name}/v", response_model=models.projects.ProjectVersion, x_properties=dict(object="project", action="newver"))
 async def post_projects_version(
     credentials: HTTPBasicCredentials = Depends(common.security_checker("post/projects/{name}/v")),
     name: str = Path(...),
-    ver: Optional[int] = Query(None),
+    ver: Optional[int] = Query(None, title="A version number. Must not exist. If omitted, the next number is allocated."),
     files: List[models.projects.ProjFile] = Body(...),
 ):
+    """Create a new project version"""
     return await models.projects.post_projects_version(credentials, name, ver, files)
 
 
-@router.patch("/{name}/v/{ver}", response_model=models.common.PatchResponse, x_properties=dict(object="projectver", action="patch"))
+@router.patch("/{name}/v/{ver}", response_model=models.common.PatchResponse, x_properties=dict(object="project", action="patchver"))
 async def patch_project_version(
     credentials: HTTPBasicCredentials = Depends(common.security_checker("patch/projects/{name}/v/{ver}")),
     name: str = Path(...),
     ver: int = Path(...),
     patch: models.projects.ProjectVersionPatchBody = Body(...),
 ):
+    """Change a project version if conditions are met"""
     return await models.projects.patch_project_version(credentials, name, ver, patch)
