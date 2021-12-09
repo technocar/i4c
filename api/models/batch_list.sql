4e8dd90c553f4c2e66480cb3e776cadeeb064481
Gyula Győr <gygyor@gmail.com>
2021-12-09 15:58:23 +0100

    api: workpiece_project in batchÜlist

--- api/models/batch_list.sql
+++ api/models/batch_list.sql
@@ -34,6 +34,36 @@ with
       and l.data_id = 'wkpcid'           /* workpiece_id, todo: use proper data */  
       and wp.batch is not null
   ),
+  map_project_version as (
+    select
+      f.savepath,
+      i.timestamp, 
+      i.project, 
+      i.real_version as version
+    from installation_file f
+    join installation i on i.id = f.installation  
+  ),
+  robot_program as (
+    select l.value_text as "program", l.timestamp, l.sequence
+    from log l
+    cross join p
+    where
+      l.timestamp >= p.after
+      and l.device='robot'
+      and l.data_id='pgm'
+  ),
+  workpiece_project as (
+    select mpv."project", mpv.version, r.timestamp, r.sequence
+    from robot_program r
+    join lateral (select 
+                    m.*,
+                    rank() over (order by m.timestamp desc) r
+                  from map_project_version m
+                  where 
+                    m.timestamp <= r.timestamp
+                 ) mpv on mpv.savepath = 'robot/'||r."program"   /* todo: use proper path */
+                          and mpv.r = 1
+  ),
   discover_log as (
     select
       case when we.timestamp is null or we.timestamp>wid.timestamp or (we.timestamp=wid.timestamp and we.sequence>wid.sequence) then wid.id end as id,
@@ -64,6 +94,18 @@ with
           or (w.timestamp = wb.timestamp and w.sequence >= wb.sequence)    
       ) a
       where a.r = 1) we on True
+    left join lateral (
+      select * from (
+        select 
+          w.*,
+          rank() over (order by w.timestamp desc, w.sequence desc) r
+        from workpiece_project as w
+        where 
+          we.timestamp is null
+          or w.timestamp < we.timestamp
+          or (w.timestamp = we.timestamp and w.sequence <= we.sequence)    
+      ) a
+      where a.r = 1) wpr on p.project is null or wpr.project = p.project
   ),
   res as (
     select 
