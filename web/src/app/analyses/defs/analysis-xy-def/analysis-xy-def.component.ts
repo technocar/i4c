917ddd4b32946dab9dc7129e05bd6fc53f63620c
Istvan Prostyak <prostyak.istvan@karatnet.hu>
2021-12-11 16:49:40 +0100

    [DEV] html tooltip

--- web/src/app/analyses/defs/analysis-xy-def/analysis-xy-def.component.ts
+++ web/src/app/analyses/defs/analysis-xy-def/analysis-xy-def.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
-import { ChartConfiguration, ChartTypeRegistry, ScatterDataPoint, BubbleDataPoint } from 'chart.js';
+import { Chart, ChartConfiguration } from 'chart.js';
 import { BehaviorSubject } from 'rxjs';
 import { ApiService } from 'src/app/services/api.service';
 import { StatData, StatVisualSettingsLegendAlign, StatVisualSettingsLegendPosition, StatXYDef, StatXYFilter, StatXYFilterRel, StatXYMeta, StatXYMetaObject, StatXYMetaObjectField, StatXYObjectType, StatXYOther } from 'src/app/services/models/api';
@@ -242,6 +242,116 @@ export class AnalysisXyDefComponent implements OnInit, AnalysisDef, AnalysisChar
       },
       options: {
         plugins: {
+          tooltip: {
+            enabled: false,
+
+              external: function(context) {
+
+                function getOrCreateTooltip(chart) {
+                  let tooltipEl = chart.canvas.parentNode.querySelector('div');
+
+                  if (!tooltipEl) {
+                    tooltipEl = document.createElement('div');
+                    tooltipEl.style.background = 'rgba(0, 0, 0, 0.7)';
+                    tooltipEl.style.borderRadius = '3px';
+                    tooltipEl.style.color = 'white';
+                    tooltipEl.style.opacity = 1;
+                    tooltipEl.style.pointerEvents = 'none';
+                    tooltipEl.style.position = 'absolute';
+                    tooltipEl.style.transform = 'translate(-50%, 0)';
+                    tooltipEl.style.transition = 'all .1s ease';
+
+                    const table = document.createElement('table');
+                    table.style.margin = '0px';
+
+                    tooltipEl.appendChild(table);
+                    chart.canvas.parentNode.appendChild(tooltipEl);
+                  }
+
+                  return tooltipEl;
+                };
+
+                  // Tooltip Element
+                const {chart, tooltip} = context;
+                const tooltipEl = getOrCreateTooltip(chart);
+
+                // Hide if no tooltip
+                if (tooltip.opacity === 0) {
+                  tooltipEl.style.opacity = 0;
+                  return;
+                }
+
+                // Set Text
+                if (tooltip.body) {
+                  const titleLines = tooltip.title || [];
+                  const bodyLines = tooltip.body.map(b => b.lines);
+
+                  const tableHead = document.createElement('thead');
+
+                  titleLines.forEach(title => {
+                    const tr = document.createElement('tr') as HTMLTableRowElement;
+                    tr.style.borderWidth = '0';
+
+                    const th = document.createElement('th') as HTMLTableCellElement;
+                    th.style.borderWidth = '0';
+                    const text = document.createTextNode(title);
+
+                    th.appendChild(text);
+                    tr.appendChild(th);
+                    tableHead.appendChild(tr);
+                  });
+
+                  const tableBody = document.createElement('tbody');
+                  bodyLines.forEach((body, i) => {
+                    const colors = tooltip.labelColors[i];
+                    console.log(body);
+
+                    const span = document.createElement('span') as HTMLSpanElement;
+                    span.style.background = colors.backgroundColor.toString();
+                    span.style.borderColor = colors.borderColor.toString();
+                    span.style.borderWidth = '2px';
+                    span.style.marginRight = '10px';
+                    span.style.height = '10px';
+                    span.style.width = '10px';
+                    span.style.display = 'inline-block';
+
+                    const tr = document.createElement('tr');
+                    tr.style.backgroundColor = 'inherit';
+                    tr.style.borderWidth = '0';
+
+                    const td = document.createElement('td');
+                    td.style.borderWidth = '0';
+
+                    const text = document.createTextNode(body.join('\n'));
+
+                    td.appendChild(span);
+                    td.appendChild(text);
+                    tr.appendChild(td);
+                    tableBody.appendChild(tr);
+                  });
+
+                  const tableRoot = tooltipEl.querySelector('table');
+
+                  // Remove old children
+                  while (tableRoot.firstChild) {
+                    tableRoot.firstChild.remove();
+                  }
+
+                  // Add new children
+                  tableRoot.appendChild(tableHead);
+                  tableRoot.appendChild(tableBody);
+                }
+
+                const {offsetLeft: positionX, offsetTop: positionY} = chart.canvas;
+
+                // Display, position, and set styles for font
+                tooltipEl.style.opacity = 1;
+                tooltipEl.style.left = positionX + tooltip.caretX + 'px';
+                tooltipEl.style.top = positionY + tooltip.caretY + 'px';
+                tooltipEl.style.font = (tooltip.options.bodyFont as any).string;
+                tooltipEl.style.padding = tooltip.options.padding + 'px ' + tooltip.options.padding + 'px';
+              }
+          },
           title: AnalysisHelpers.setChartTitle(data.stat_def.xydef.visualsettings?.title),
           subtitle: AnalysisHelpers.setChartTitle(data.stat_def.xydef.visualsettings?.subtitle),
           legend: {
@@ -269,7 +379,7 @@ export class AnalysisXyDefComponent implements OnInit, AnalysisDef, AnalysisChar
                   return xIsCategory ? xLabels[value] : value;
               }
             }
-          }
+          },
         }
       }
     };
