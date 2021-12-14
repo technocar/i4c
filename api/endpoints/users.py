6fe05aa153f9bf1b3db623d0098e9dfef6078fb3
Gyula Győr <gygyor@gmail.com>
2021-12-14 16:12:50 +0100

    api: get/users/{name}

--- api/endpoints/users.py
+++ api/endpoints/users.py
@@ -1,5 +1,5 @@
 from typing import List, Optional
-from fastapi import Query, Depends
+from fastapi import Query, Depends, Path, HTTPException
 from fastapi.responses import PlainTextResponse
 from fastapi.security import HTTPBasicCredentials
 import models.users
@@ -20,5 +20,17 @@ async def get_users(
     active_only: Optional[bool] = Query(True)
 ):
     return await models.users.get_users(credentials, active_only=active_only)
+
+
+@router.get("/{name}", response_model=models.users.User, x_properties=dict(object="users", action="get"))
+async def get_user(
+    credentials: HTTPBasicCredentials = Depends(common.security_checker("get/users/{name}")),
+    name: str = Path(...)
+):
+    res = await models.users.get_users(credentials, name, active_only=False)
+    if res is None:
+        raise HTTPException(status_code=404, detail="No record found")
+    return res[0]
+
 
 
