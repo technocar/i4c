831088822fce799230869c03c37291d6c210111d
Gyula Győr <gygyor@gmail.com>
2021-12-14 14:41:28 +0100

    api: get/roles

--- api/models/roles.py
+++ api/models/roles.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
+from textwrap import dedent
 from typing import List
-from common import I4cBaseModel
+from common import I4cBaseModel, DatabaseConnection
 
 path_list = []
 
@@ -17,8 +18,33 @@ class Role(I4cBaseModel):
 
 
 async def get_roles(credentials):
-    # todo 1: *****
-    pass
+    async with DatabaseConnection() as conn:
+        res = []
+        sql = dedent("""\
+                select 
+                  r.name,
+                  coalesce(rs.subroles, array[]::varchar[]) subroles,
+                  rg.endpoint,
+                  rg.features
+                from "role" r
+                left join (select
+                        rs.role, array_agg(rs.subrole) subroles
+                      from role_subrole rs
+                      group by rs.role
+                     ) rs on rs.role = r.name
+                left join role_grant rg on rg.role = r.name
+                where r."status" = 'active'
+                order by r.name
+            """)
+        d = await conn.fetch(sql)
+        current_role = None
+        for r in d:
+            if current_role is None or (r[0] != current_role.name):
+                current_role = Role(name=r[0], subroles=r[1], privs=[])
+                res.append(current_role)
+            if r[2] is not None:
+                current_role.privs.append(Priv(endpoint=r[2], features=r[3]))
+        return res
 
 
 async def get_priv(credentials):
