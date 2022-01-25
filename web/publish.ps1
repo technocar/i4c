320667f0a0e22a7c24e46f6d677d28f4ed99db64
Istvan Prostyak <prostyak.istvan@karatnet.hu>
2022-01-25 17:27:08 +0100

    [CHG] relative path

--- web/publish.ps1
+++ web/publish.ps1
@@ -1,10 +1,10 @@
-$src_api_path="D:\work\technocar\machinestuff\i4c\api\"
-$src_web_path="D:\work\technocar\machinestuff\i4c\web\dist\"
+$src_api_path="..\api\"
+$src_web_path=".\dist\"
 $dest_api_path="\\karatnetsrv\Karatnet\machinestuff\i4c\api\"
 $dest_web_path="\\karatnetsrv\Karatnet\machinestuff\i4c\web\"
 $dest_iis_path="\\karatnetsrv\i4c_web\"
 
-Invoke-Expression "ng build --configuration=production"
+#Invoke-Expression "ng build --configuration=production"
 
 Copy-Item -Path $src_api_path"*" -Destination $dest_api_path -Exclude @("__pycache__", "internal_file", "log", "*.yaml", "*.cmd") -Force -Confirm:$false -Recurse
 Remove-Item $dest_web_path"*" -Force -Confirm:$false -Recurse
