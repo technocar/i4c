b8cde99fa24e6c5d30ed293372623cd17dec542e
Istvan Prostyak <prostyak.istvan@karatnet.hu>
2022-01-25 17:16:30 +0100

    [FIX] if feature is not set there isn't checked

--- web/src/app/services/auth.service.ts
+++ web/src/app/services/auth.service.ts
@@ -69,6 +69,6 @@ export class AuthenticationService {
       return false;
 
     var privs = this.currentUserValue?.privs ?? [];
-    return (privs.find((p) => p.endpoint === endpoint && ((p.features ?? []).length === 0 || p.features.indexOf(privilige) > -1 ))) !== undefined;
+    return (privs.find((p) => p.endpoint === endpoint && ((p.features ?? []).length === 0 || !privilige || p.features.indexOf(privilige) > -1 ))) !== undefined;
   }
 }
