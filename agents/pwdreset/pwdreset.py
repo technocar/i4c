6ba1ea0779030bb4f2b507763e583e7738f325d7
Krisztián Pintér <pinterkr@gmail.com>
2022-02-01 18:26:10 +0100

    v1

--- agents/pwdreset/pwdreset.py
+++ agents/pwdreset/pwdreset.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import base64
 import yaml
 import logging.config
 import smtplib
@@ -11,6 +12,7 @@ log = None # will be filled in from main
 
 
 def fail(msg, exit_code=1):
+    global log
     if msg is not None:
         print(f"{msg}")
     log.debug(f"fail {exit_code} {msg}")
@@ -27,6 +29,7 @@ def main():
     if "log" in conf:
         logging.config.dictConfig(conf["log"])
 
+    global log
     log = logging.getLogger()
     log.debug("start")
 
@@ -36,28 +39,35 @@ def main():
     log.debug(f"profile: {profile}")
 
     svr = conf.get("smtp-server", None)
-    uid = conf.get("smtp-server", None)
+    port = conf.get("smtp-port", None)
+    uid = conf.get("smtp-user", None)
     pwd = conf.get("smtp-password", None)
-    mail_from = conf.get("smtp-from", None)
-    smtp_protocol = conf.get("smtp-from", "starttls")
+    sender = conf.get("smtp-from", None)
+    protocol = conf.get("smtp-protocol", "starttls")
 
     log.debug("accessing profile")
     conn = i4c.I4CConnection(profile=profile)
     extra = conn.profile_data.get("extra", {})
     svr = svr or extra.get("smtp-server", None)
+    port = port or extra.get("smtp-port", None)
     uid = uid or extra.get("smtp-user", None)
     pwd = pwd or extra.get("smtp-password", None)
-    mail_from = mail_from or extra.get("smtp-from", None)
-    smtp_protocol = smtp_protocol or extra.get("smtp-protocol", None)
+    sender = sender or extra.get("smtp-from", None)
+    protocol = protocol or extra.get("smtp-protocol", None)
 
     if svr is None:
         log.debug("no server is given, falling back to localhost")
         svr = "127.0.0.1"
     
-    if smtp_protocol not in ("tls", "starttls", "plain"):
-        fail(f"Smtp protocol must be tls, starttls or plain. '{smtp_protocol}' was given.")
+    if protocol not in ("ssl", "starttls", "plain"):
+        fail(f"Smtp protocol must be tls, starttls or plain. '{protocol}' was given.")
 
-    with open("pwdreset.html", "r") as f:
+    log.debug(f"smtp settings: {protocol} {svr}:{port} user:{uid} from:{sender}")
+
+    if not os.path.isfile("pwdreset.template"):
+        fail("Missing pwdreset.template")
+
+    with open("pwdreset.template", "r", encoding="utf-8") as f:
         tmpl = f.read()
 
     env = jinja2.Environment()
@@ -69,6 +79,8 @@ def main():
     except Exception as e:
         fail(e)
 
+    sep = base64.urlsafe_b64encode(os.urandom(15)).decode()
+
     log.debug(f"got {len(resets)} items")
     for r in resets:
         email = r["email"]
@@ -77,15 +89,24 @@ def main():
         log.info(f"sending to {email} for {login}")
 
         log.debug("rendering")
-        html = tmpl.render(email=email, token=token, login=login)
-        print(f"sending to {email}")
-        print(html)
+        mail_body = tmpl.render(email=email, token=token, login=login, sep=sep, sender=sender)
+        mail_body = mail_body.encode("utf-8")
 
         try:
-            mailer = smtplib.SMTP(svr)
-            mailer.login(uid, pwd)
-            # TODO send email
-            raise Exception("SENDING NOT IMPLEMENTED")
+            log.debug("connecting")
+            if protocol == "ssl":
+                mailer = smtplib.SMTP_SSL(svr, port=port)
+            elif protocol == "starttls":
+                mailer = smtplib.SMTP(svr, port=port)
+                mailer.starttls()
+            else:
+                mailer = smtplib.SMTP(svr, port=port)
+            if uid:
+                log.debug("authenticating")
+                mailer.login(uid, pwd)
+
+            log.debug("sending")
+            mailer.sendmail(sender, [email], mail_body)
 
             try:
                 log.debug("marking as sent")
@@ -94,9 +115,10 @@ def main():
                 log.error(f"Failed to mark {login} as sent: {e}")
 
         except Exception as e:
-            log.error(f"send fail {email} for {login}")
+            log.error(f"failed mailing to {email} for {login}: {e}")
 
     log.debug("finished")
+
 
 if __name__ == '__main__':
     main()
