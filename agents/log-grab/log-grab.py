89c741a9a357d212326305f2592e18e1ef08e011
Vas József <jvas@karatnet.hu>
2022-02-10 13:31:50 +0100

    *: format changes in source files

--- agents/log-grab/log-grab.py
+++ agents/log-grab/log-grab.py
@@ -1,15 +1,13 @@
 
 import os
+import time
 import csv
 import shutil
 import datetime
 import copy
 import logging.config
 import yaml
-import i4c
-
-# TODO workpiece id should be taken from the file name
-#   and written to the log as data_id = "wkpcid"
+import cli.i4c
 
 robot_actions = {
     "Darab beérkezett": "spotted",
@@ -33,7 +31,7 @@ robot_actions = {
     "Darab lerakva szalagra": "place_good_out",
     "Darab lerakva minta fiókba": "place_sample_out",
     "Darab lerakva NOK tárolóba": "place_bad_out",
-    "Folyamat megszakítva": "stopped"} # TODO add gom repair statuses
+    "Folyamat megszakítva": "stopped"}  # TODO add gom repair statuses
 
 with open("log-grab.yaml") as f:
     cfg = yaml.load(f, Loader=yaml.FullLoader)
@@ -43,7 +41,7 @@ with open("log-grab.yaml") as f:
 log = logging.getLogger()
 
 profile = cfg.get("profile", None)
-conn = i4c.I4CConnection(profile=profile)
+conn = cli.i4c.I4CConnection(profile=profile)
 
 
 def check_params(paths):
@@ -87,26 +85,28 @@ def process_robot(section):
 
     src_path = params["source-path"]
 
-    files = [f for f in os.listdir(src_path) if
-             os.path.isfile(os.path.join(src_path, f)) and f.upper().endswith(".CSV")]
+    files = [entry for entry in os.listdir(src_path) if
+             os.path.isfile(os.path.join(src_path, entry)) and entry.upper().endswith("FINISH.CSV")]
     if len(files) == 0:
         log.debug("No files to load")
         return
-    for f in files:
+    for currentfile in files:
         wkpcid = None
         progid = None
         api_params_array = []
-        log.debug("Processing file %s", f)
-        with open(os.path.join(src_path, f)) as csvfile:
+        log.debug("Processing file %s", currentfile)
+        with open(os.path.join(src_path, currentfile)) as csvfile:
             csvreader = csv.reader(csvfile, delimiter=";", quotechar=None)
             for lines in csvreader:
                 if len(lines) != 2:
+                    if lines[0].upper() == "Naplózott események".upper():
+                        continue
                     log.error("Line %d in rong format!", [csvreader.line_num])
                     return
 
                 if lines[0].upper() == "Munkadarab azonosítója".upper():
                     wkpcid = lines[1]
-                elif lines[0].upper() == "Kiválasztott program neve".upper():
+                elif lines[0].upper() == "Program neve".upper():
                     progid = lines[1]
                 elif csvreader.line_num >= 5:
                     if csvreader.line_num == 5:
@@ -128,7 +128,8 @@ def process_robot(section):
                         api_params_array.append(copy.deepcopy(api_params))
 
                     api_params["sequence"] += 1
-                    api_params["timestamp"] = datetime.datetime.strptime(lines[0], "%Y.%m.%d %H:%M:%S").astimezone()
+                    api_params["timestamp"] = datetime.datetime.strptime(lines[0], "%Y.%m.%d %H:%M:%S").strftime(
+                            "%Y-%m-%dT%H:%M:%SZ")
                     api_params["data_id"] = robot_actions.get(lines[1], "other")
                     if api_params["data_id"] == "other":
                         api_params["value_text"] = lines[1]
@@ -139,7 +140,7 @@ def process_robot(section):
             csvfile.close()
         conn.invoke_url("log", "POST", api_params_array)
         log.debug("Archiving file...")
-        shutil.move(os.path.join(src_path, f), os.path.join(params["archive-path"], f))
+        shutil.move(os.path.join(src_path, currentfile), os.path.join(params["archive-path"], currentfile))
 
 
 def process_GOM(section):
@@ -151,40 +152,61 @@ def process_GOM(section):
         "device": "GOM",
         "instance": 0,
         "data_id": '',
-        "value": '',
-        "value_num": 0,
+        "value": None,
+        "value_num": None,
         "value_text": None,
         "value_extra": None,
         "value_add": None
     }
 
     params = check_params(section)
-    if params["OK"]:
+    if not params["OK"]:
         return
 
     src_path = params["source-path"]
 
-    files = [f for f in os.listdir(src_path) if os.path.isfile(os.path.join(src_path, f))
-             and any(f.upper().endswith(ext) for ext in (".CSV", ".ATOS" ".PDF")) ]
+    files = [entry for entry in os.listdir(src_path) if os.path.isfile(os.path.join(src_path, entry))
+             and any(entry.upper().endswith(ext) for ext in (".CSV", ".ATOS" ".PDF"))]
     if len(files) == 0:
         log.debug("no files to load")
         return
 
-    for f in files:
-        log.debug("processing file %s", f)
-        fname = os.path.splitext(f)[0]
-
-        if f.upper().endswith(".CSV"):
+    for currentfile in files:
+        log.debug("processing file %s", currentfile)
+        fname, fext = os.path.splitext(currentfile)
+        if fext.upper() == ".CSV":
+            if not os.path.exists(os.path.join(src_path, fname + '.LOG')):
+                log.error("No log file found")
+                continue
             api_params_array = []
-            api_params["sequence"] = 0
-            api_params["timestamp"] = datetime.datetime.strptime(fname[-14:], "%Y%m%d%H%M%S").strftime(
-                "%Y-%m-%dT%H:%M:%SZ")
-            api_params["data_id"] = "pgm"
-            api_params["value_text"] = fname[:-15]
-            api_params_array.append(copy.deepcopy(api_params))
-            api_params["value_text"] = ''
 
-            with open(os.path.join(src_path, f)) as csvfile:
+            with open(os.path.join(src_path, fname + ".log")) as csvfile:
+                api_params["sequence"] = 0
+                api_params["data_id"] = "GOM_log"
+                for (line_no, lines) in enumerate(csvfile):
+                    (part1, part2, part3, part4) = lines.split(' ', 3)
+                    if part3 == "*" : api_params["data_id"] = "WARNING"
+                    elif part3 == "!" : api_params["data_id"] = "ERROR"
+                    else: api_params["data_id"] = "INFO"
+
+                    api_params["timestamp"] = datetime.datetime.strptime(part1 + ' ' + part2, "%Y-%m-%d %H:%M:%S.%f").strftime(
+                            "%Y-%m-%dT%H:%M:%SZ.%f")
+
+                    if line_no == 0:
+                        first_time = api_params["timestamp"]
+
+                    api_params["value_text"] = part4.strip()
+                    api_params_array.append(copy.deepcopy(api_params))
+                    api_params["sequence"] += 1
+                csvfile.close()
+
+            api_params["value_text"] = '_'.join(fname.split('_')[0:2])
+            api_params["data_id"] = "pgm"
+            api_params["timestamp"] = first_time
+            api_params_array.append(copy.deepcopy(api_params))
+            api_params["value_text"] = None
+
+            with open(os.path.join(src_path, currentfile)) as csvfile:
                 csvreader = csv.reader(csvfile, delimiter=";", quotechar=None)
                 for lines in csvreader:
                     if csvreader.line_num == 1:
@@ -204,9 +226,19 @@ def process_GOM(section):
             with open(os.path.join(src_path, f), "rb") as datafile:
                 conn.invoke_url("intfiles/v/1/" + f, "PUT", datafile)
                 datafile.close()
+            pass
 
         log.debug("archiving file")
-        shutil.move(os.path.join(src_path, f), os.path.join(params["archive-path"], f))
+        shutil.move(os.path.join(src_path, currentfile), os.path.join(params["archive-path"], currentfile))
+        if fext.upper() == ".CSV":
+            shutil.move(os.path.join(src_path, fname + '.log'), os.path.join(params["archive-path"], fname + '.log'))
+
+        for newext in ['.ok', '.error', '.bad', '.good']:
+            fnew = fname + newext
+            if os.path.exists(os.path.join(src_path, fnew)):
+                log.debug("archiving additional file {}".format(fnew))
+                shutil.move(os.path.join(src_path, fnew), os.path.join(params["archive-path"], fnew))
+
 
 
 log.info("start")
