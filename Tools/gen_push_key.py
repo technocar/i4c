ded7042a3a76de6d49de7e6219a0b7eee9d90d7c
Krisztián Pintér <pinterkr@gmail.com>
2022-02-18 15:20:30 +0100

    nicening + reflect to module changes

--- Tools/gen_push_key.py
+++ Tools/gen_push_key.py
@@ -1,9 +1,9 @@
 import sys
 import i4c
+import json
 from urllib.error import HTTPError
 from py_vapid.utils import b64urlencode
 from py_vapid import Vapid
-
 from cryptography.hazmat.primitives import serialization
 
 i4c_conn = None
@@ -14,29 +14,27 @@ def init_globals():
     global i4c_conn
     global overwrite_key
 
-    overwrite_key = not (next((opt for opt in sys.argv if opt == "--overwrite"), None) is None)
+    overwrite_key = "--overwrite" in sys.argv
     i4c_conn = i4c.I4CConnection(profile=None)
 
 
 def main():
     vapid = Vapid()
     vapid.generate_keys()
-    priv_key = b"".join(ln for ln in (vapid.private_pem().split(b"\n")) if not ln.startswith(b"--") and ln != b"").decode()
-    pub_key = b64urlencode(vapid.public_key.public_bytes(serialization.Encoding.X962,
-                                            serialization.PublicFormat.UncompressedPoint
-    ))
+
+    priv_key = (ln for ln in vapid.private_pem().split(b"\n") if not ln.startswith(b"--") and ln != b"")
+    priv_key = b"".join(priv_key).decode()
+
+    pub_key = vapid.public_key
+    pub_key = pub_key.public_bytes(serialization.Encoding.X962,
+                                   serialization.PublicFormat.UncompressedPoint)
+    pub_key = b64urlencode(pub_key)
 
     try:
         key_exists = i4c_conn.settings.get(key="push_priv_key") is not None
         key_exists = key_exists or i4c_conn.settings.get(key="push_public_key") is not None
-    except HTTPError as err:
-        if not err.fp is None:
-            print(err.code, err.fp.read())
-        else:
-            print(err.code, err.msg)
-        exit(1)
     except Exception as err:
-        print(f"Error: {err}")
+        print(f"{err}")
         exit(1)
 
     if not key_exists or overwrite_key:
@@ -45,13 +43,8 @@ def main():
             i4c_conn.settings.set(key="push_public_key", body={"value": pub_key})
             print(f"Keys are set")
             exit(0)
-        except HTTPError as err:
-            if not err.fp is None:
-                print(err.code, err.fp.read())
-            else:
-                print(err.code, err.msg)
         except Exception as err:
-            print(f"Error: {err}")
+            print(f"{err}")
             exit(2)
     else:
         print(f"Keys already set. Add --overwrite if you wish.")
