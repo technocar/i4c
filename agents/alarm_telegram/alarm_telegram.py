5fd8243ec4c4b2e2406898e90fa6f3f28eedbe4c
Vas József <jvas@karatnet.hu>
2022-02-24 11:11:33 +0100

    *: more cleanup

--- agents/alarm_telegram/alarm_telegram.py
+++ agents/alarm_telegram/alarm_telegram.py
@@ -13,13 +13,27 @@ from telegram.ext.dispatcher import Dispatcher
 from telegram.update import Update
 from telegram.ext.callbackcontext import CallbackContext
 
-cfg = None
-log = None
+cfg: dict = None
+log: logging.Logger = None
 poll = 0
-i4c_conn = None
+i4c_conn: i4c.I4CConnection = None
 bot: telegram.Bot = None
-updater = None
+updater: Updater = None
 dispatcher: Dispatcher = None
+
+
+def is_accessible(telegram_user: telegram.User, function: str, chat_id: str) -> bool:
+    global log
+
+    #    TODO: Add code to check if the current user has the right to execute command
+    #    Telegram user must be mapped to i4c user
+
+    result = True
+    if not result:
+        log.error(f"Telegram user {telegram_user.username} doesn't have the right to {function}")
+        bot.send_message(chat_id=chat_id, parse_mode=telegram.constants.PARSEMODE_HTML,
+                         text="Nincs hozzá joga")
+    return result
 
 
 def init_bot():
@@ -29,40 +43,41 @@ def init_bot():
     global dispatcher
 
     def my_user_id(update: Update, context: CallbackContext):
-        context.bot.send_message(chat_id=update.effective_chat.id,
-                                 parse_mode=telegram.constants.PARSEMODE_HTML,
-                                 text=f"Az ön azonosítója: {update.effective_user.id}")
+        if is_accessible(update.effective_user, "my_user_id", update.effective_chat.id):
+            bot.send_message(chat_id=update.effective_chat.id,
+                             parse_mode=telegram.constants.PARSEMODE_HTML,
+                             text=f"Az ön azonosítója: {update.effective_user.id}")
 
     def my_chat_id(update: Update, context: CallbackContext):
-#        rslt = context.bot.get_chat_member(chat_id=update.effective_chat.id, user_id=update.effective_user.id)
-        context.bot.send_message(chat_id=update.effective_chat.id,
-                                 parse_mode=telegram.constants.PARSEMODE_HTML,
-                                 text=f"A chat szoba azonosítója: {update.effective_chat.id}")
+        # rslt = context.bot.get_chat_member(chat_id=update.effective_chat.id, user_id=update.effective_user.id)
+        if is_accessible(update.effective_user, "my_chat_id", update.effective_chat.id):
+            bot.send_message(chat_id=update.effective_chat.id,
+                             parse_mode=telegram.constants.PARSEMODE_HTML,
+                             text=f"A chat szoba azonosítója: {update.effective_chat.id}")
 
     def command(update: Update, context: CallbackContext):
-        context.bot.send_message(chat_id=update.effective_chat.id,
-                                 parse_mode=telegram.constants.PARSEMODE_HTML,
-                                 text="""Üdvözli az i4c telegram bot!
+        if is_accessible(update.effective_user, "command", update.effective_chat.id):
+            bot.send_message(chat_id=update.effective_chat.id,
+                             parse_mode=telegram.constants.PARSEMODE_HTML,
+                             text="""Üdvözli az i4c telegram bot!
                                  
 <b>command</b> - Elérhető parancsok listája
 <b>mychatid</b> - Ennek a chat szobának az azonosítója
 <b>myuserid</b> - Az Ön azonosítója
 <b>logo</b> - TechnoCar logo""")
 
-    def logo(update: Update, context: CallbackContext, user_data, chat_data):
-        context.bot.send_photo(chat_id=update.effective_chat.id,
-                               photo="http://www.technocar.hu/assets/images/tc-logo-121x117.jpg")
-
-
-
-    log.debug("getting telegram API key")
-    api_key = i4c_conn.invoke_url('settings/telegram_api_key')
+    def logo(update: Update, context: CallbackContext):
+        if is_accessible(update.effective_user, "logo", update.effective_chat.id):
+            bot.send_photo(chat_id=update.effective_chat.id,
+                           photo="http://www.technocar.hu/assets/images/tc-logo-121x117.jpg")
 
     if bot is None:
+        log.debug("getting telegram API key")
+        api_key = i4c_conn.invoke_url('settings/telegram_api_key')
         bot = telegram.Bot(token=api_key)
 
     if updater is None:
-        updater = Updater(api_key, use_context=True)
+        updater = Updater(bot=bot, use_context=True)
         dispatcher = updater.dispatcher
         dispatcher.add_handler(CommandHandler(command="mychatid", callback=my_chat_id))
         dispatcher.add_handler(CommandHandler(command="myuserid", callback=my_user_id))
@@ -76,7 +91,7 @@ def init_globals():
     global log
     global poll
     global i4c_conn
-
+    
     with open("alarm_telegram.conf", "r") as f:
         cfg = yaml.load(f, Loader=yaml.FullLoader)
 
@@ -102,30 +117,24 @@ def init_globals():
     i4c_conn = i4c.I4CConnection(profile=profile)
 
 
-
-def set_status(id, status):
-    try:
-        log.debug(f"marking as {status}")
-        chg = {"conditions": [{"status": ["outbox"]}], "change": {"status": status}}
-        i4c_conn.invoke_url(f'alarm/recips/{id}', 'PATCH', jsondata=chg)
-    except Exception as e:
-        log.error(f"error while marking as {status}: {e}")
-
-
-def set_backoff(id, fail_count, backoff):
-    try:
-        log.debug(f"setting backoff")
-        chg = {"conditions": [{"status": ["outbox"]}],
-               "change": {"backoff_until": backoff.isoformat(), "fail_count": fail_count}}
-        i4c_conn.invoke_url(f'alarm/recips/{id}', 'PATCH', jsondata=chg)
-    except Exception as e:
-        log.error(f"error while setting backoff: {e}")
-
-
 def main():
-    global bot
-    global updater
 
+    def set_status(id, status):
+        try:
+            log.debug(f"marking as {status}")
+            chg = {"conditions": [{"status": ["outbox"]}], "change": {"status": status}}
+            i4c_conn.invoke_url(f'alarm/recips/{id}', 'PATCH', jsondata=chg)
+        except Exception as e:
+            log.error(f"error while marking as {status}: {e}")
+
+    def set_backoff(id, fail_count, backoff):
+        try:
+            log.debug(f"setting backoff")
+            chg = {"conditions": [{"status": ["outbox"]}],
+                   "change": {"backoff_until": backoff.isoformat(), "fail_count": fail_count}}
+            i4c_conn.invoke_url(f'alarm/recips/{id}', 'PATCH', jsondata=chg)
+        except Exception as e:
+            log.error(f"error while setting backoff: {e}")
 
     while True:
         init_bot()
@@ -156,6 +165,7 @@ def main():
         time.sleep(poll)
         log.debug("finished")
 
+
 if __name__ == '__main__':
     try:
         init_globals()
@@ -165,4 +175,3 @@ if __name__ == '__main__':
     except Exception as e:
         log.error(f"error: {e}")
         raise
-
