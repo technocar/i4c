cf1daf901114d18298bdc51bd41d8a3cc537fff0
Vas József <jvas@karatnet.hu>
2022-03-03 15:01:42 +0100

    +: uses CancelOnDisconnect

--- api/endpoints/log.py
+++ api/endpoints/log.py
@@ -6,7 +6,7 @@ from starlette.requests import Request
 from starlette.responses import Response
 import models.log
 import common
-from I4cAPI import I4cApiRouter
+from I4cAPI import I4cApiRouter, CancelOnDisconnect
 from common.exceptions import I4cClientNotFound
 from models import Device, DeviceAuto
 
@@ -29,6 +29,7 @@ async def snapshot(
             summary="Search the log.")
 async def find(
         request: Request,
+        disconnector: CancelOnDisconnect = Depends(CancelOnDisconnect),
         credentials: HTTPBasicCredentials = Depends(common.security_checker("get/log/find")),
         device: Device = Query(..., title="Device."),
         timestamp: Optional[datetime] = Query(None, title="Around timestamp, iso format."),
@@ -42,11 +43,10 @@ async def find(
         rel: Optional[str] = Query(None, title="Relation for the val or extra.")
 ):
     """List log entries."""
-    rs = await models.log.get_find(credentials, device, timestamp, sequence, before_count, after_count, categ, data_id, val, extra, rel)
+    rs = await disconnector(models.log.get_find(credentials, device, timestamp, sequence, before_count, after_count, categ, data_id, val, extra, rel))
     if rs is None:
         raise I4cClientNotFound("No log record found")
     return rs
-
 
 @router.get("/meta", response_model=List[models.log.Meta], operation_id="log_meta", summary="Get log metadata.")
 async def meta(
