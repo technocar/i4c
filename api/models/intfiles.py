e0abd0e3f6d44f40c76652165a6ed7a2984f73a0
Krisztián Pintér <pinterkr@gmail.com>
2022-04-01 14:17:09 +0200

    reorg again

--- api/models/intfiles.py
+++ api/models/intfiles.py
@@ -111,30 +111,45 @@ async def intfiles_check_usage(ver: int, name: str, *, pconn=None):
         return id
 
 
+def cleanup_file(file_name):
+    if file_name and os.path.isfile(file_name):
+        try:
+            os.remove(file_name)
+        except:
+            pass
+
+
 async def intfiles_put(
     request: Request,
     credentials: HTTPBasicCredentials,
     ver: int,
     name: str
 ):
+    temp_name = None
+    fn = None
+    try:
+        temp_name = get_internal_file_name("." + secrets.token_hex(16))
+        with open(temp_name, "wb") as tf:
+            hash_obj = sha384()
+            async for chunk in request.stream():
+                hash_obj.update(chunk)
+                tf.write(chunk)
+            hash = hash_obj.hexdigest()
+
+        fn = get_internal_file_name(hash)
+        existed = os.path.isfile(fn)
+        if existed:
+            os.remove(fn)
+        os.rename(temp_name, fn)
+    except starlette.requests.ClientDisconnect as e:
+        return
+    finally:
+        cleanup_file(temp_name)
+
     async with DatabaseConnection() as conn:
         async with conn.transaction(isolation='repeatable_read'):
-            temp_name = get_internal_file_name("." + secrets.token_hex(16))
-            fn = None
             try:
                 id = await intfiles_check_usage(ver, name, pconn=conn)
-                with open(temp_name, "wb") as tf:
-                    hash_obj = sha384()
-                    async for chunk in request.stream():
-                        hash_obj.update(chunk)
-                        tf.write(chunk)
-                    hash = hash_obj.hexdigest()
-
-                fn = get_internal_file_name(hash)
-                if os.path.isfile(fn):
-                    os.remove(fn)
-                os.rename(temp_name, fn)
-
                 if id:
                     sql_update = dedent("""\
                         update file_int
@@ -146,20 +161,9 @@ async def intfiles_put(
                     sql_insert = "insert into file_int (name, ver, content_hash) values ($1, $2, $3)"
                     await conn.execute(sql_insert, name, ver, hash)
             except Exception as e:
-                if fn and os.path.isfile(fn):
-                    try:
-                        os.remove(fn)
-                    except:
-                        pass
-                if os.path.isfile(temp_name):
-                    try:
-                        os.remove(temp_name)
-                    except:
-                        pass
-                if isinstance(e, starlette.requests.ClientDisconnect):
-                    return
-                else:
-                    raise
+                if not existed:
+                    cleanup_file(fn)
+                raise
 
 
 async def intfiles_delete(credentials, ver, name):
